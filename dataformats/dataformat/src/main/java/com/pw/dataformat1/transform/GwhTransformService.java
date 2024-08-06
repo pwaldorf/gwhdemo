2bb719081211f07850b6ef6ce03af46ca25ab6ee
philip waldorf <philip.waldorf@gmail.com>
2024-08-06 15:44:09 -0400

    MVP

--- dataformats/dataformat/src/main/java/com/pw/dataformat1/transform/GwhTransformService.java
+++
@@ -1,8 +0,0 @@
-package com.pw.dataformat1.transform;
-
-import java.util.Map;
-
-public interface GwhTransformService<K, V> {
-    public String getName();
-    public Map<K, V> process(String rawMessage, String template, Boolean isHeader) throws Exception;
-}
--- gwhservice/src/main/java/com/pw/gwhservice1/gwhposting/BuzService.java
+++
@@ -1,7 +0,0 @@
-package com.pw.gwhservice1.gwhposting;
-
-import com.pw.gwhservice1.model.Entity;
-
-public interface BuzService {
-    Entity process(Entity msg) throws RuntimeException;
-}
